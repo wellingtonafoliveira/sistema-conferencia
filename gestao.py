c28b6981a335d505015e6b4d17e5e4ca0ea89430
wellingtonafoliveira <76909652+wellingtonafoliveira@users.noreply.github.com>
2026-04-03 16:02:17 -0300

    Update gestao.py

--- gestao.py
+++ gestao.py
@@ -2,15 +2,15 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 
-st.title("📊 Painel de Gestão")
+st.title("📊 Gestão de Cargas")
 
 conn = sqlite3.connect("banco.db")
 df = pd.read_sql("SELECT * FROM cargas", conn)
 
 st.metric("Total", len(df))
 st.metric("Pendentes", len(df[df["status"]=="PENDENTE"]))
-st.metric("Finalizados", len(df[df["status"]=="FINALIZADO"]))
-st.metric("Divergentes", len(df[df["status"]=="DIVERGENTE"]))
+st.metric("OK", len(df[df["status"]=="OK"]))
+st.metric("Divergente", len(df[df["status"]=="DIVERGENTE"]))
 
 st.dataframe(df)
 
@@ -19,18 +19,16 @@ st.subheader("🔄 Reabrir DT")
 dt = st.text_input("DT")
 
 if st.button("Reabrir"):
+
     df_dt = pd.read_sql(f"SELECT * FROM cargas WHERE dt='{dt}'", conn)
 
     if df_dt.empty:
         st.error("DT não encontrada")
     else:
-        if "FINALIZADO" in df_dt["status"].values:
-            st.error("Não pode reabrir DT finalizada sem divergência")
+        if "OK" in df_dt["status"].values:
+            st.error("Não pode reabrir conferência sem divergência")
         else:
-            conn.execute(f"""
-            UPDATE cargas SET status='EM_ANDAMENTO'
-            WHERE dt='{dt}'
-            """)
+            conn.execute(f"UPDATE cargas SET status='PENDENTE' WHERE dt='{dt}'")
             conn.commit()
             st.success("Reaberta com sucesso")
 
